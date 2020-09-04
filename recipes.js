dbd4b80260dd7734c14bbfe3acdcf82d18388c72
Taliiu <taleacosmia@web.de>
2020-09-04 22:58:59 +0200

    more change

--- recipes.js
+++ recipes.js
@@ -2,31 +2,68 @@ const recipes = [
 	{
 		id: "1",
 		domain: "https://somedomainname",
-		name: "name",
+		name: "Budda Bowl",
+		servings: "unknown",
 		categories: ["vegan", "healthy"],
 		content: {
-			ingredients: ["some", "more", "ingredients"],
-			method: "Some Text"
+			ingredients: ["spring onions", "garlic", "carrots","avocado", "spinach", "other vegtables you like", "coconut milk", "peanutbutter", "vegtable oil", "soy sauce"],
+			instruction: ["Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab soluta ducimus et esse numquam fuga, omnis modi ratione id m"]
 		}
 	},
 	{
 		id: "2",
 		domain: "https://anotherdomainname",
-		name: "name2",
+		name: "Gado Gado",
+		servings: "4",
 		categories: ["healthy", "vegan"],
 		content: {
-			ingredients: ["some", "more", "ingredients"],
-			method: "Some Other Text"
+			ingredients: ["2 medium red onions, diced", "4 cloves garlic, thinly sliced", "180g mock chicken meat replacement", "4 big handfuls pf green beans, cut in half", "2 red bell peppers", "7 sun-dried tomatoes(1/3cup), chopped", "80ml sodium-reduced soy sauce", "2 tsp sambal olek(more if needed)"],
+			instruction: ["1. To a large pan on high heat add the oil, onion and garlic, sauteing for 5 minutes. Add a small splash of water as needed to deglaze the pan.", 
+			"2. Add the mock chicken pieces. Continue to saute until lightly golden. About 5 minutes. Remove from the pan and transfer to a bowl.",
+			"3. In the same empty pan, add 1 cup of boiling water and the green beans. Cover with a lid and reduce the temperature so the beans simmer in the water. The steam generated will cook the beans, even if they are not completely submerged. Cook for about 7 minutes", 
+			"4. Back to the pan, add the bell pepper, cover, and cook for another 5 minutes.  It's okay if about 1-2 Tbsp of water remains at the end but if there is any excess, drain it.", 
+			"5. Add back to the pan the onion/chicken mixture, sundried tomatoes, soy sauce and sambal. Stir for a couple minutes until everything is heated through. Serve with rice or other grain of choice, top with the peanut sauce, and garnish with some cilantro. Enjoy!"]
 		}
 	},
 	{
 		id: "3",
 		domain: "https://another2domainname",
-		name: "name3",
+		servings: "2",
+		name: "Filled butternut squash",
 		categories: ["healthy", "vegan"],
 		content: {
-			ingredients: ["some", "more", "ingredients"],
-			method: "Some More Text"
-		}
-	}
+			ingredients: ["1/2 cup brown rice", "1 cup vegtable stock", "2 cloves garlic", "2 tomatoes", "1/2 cup black beans", "1/2 cup sweetcorn", "1/2 cup cilantro", "1 tsp cumin", "1 tsp chili powder", "1 tsp salt"],
+			instruction: ["1. Cut butternut squash in half and roast for 20-35min in the oven (200°C)",
+						"2. Add rice and vegtable stock to a pan, put the lit on and cook until the rice has absorbed all the water",
+						"3. Add all vegtables, herbs and spices to the pan. Take the squash out of the oven and scrape out the flesh and add to the pan as well",
+						"4. Mix everything together and put the mixture back into the remainig skin of the squash",
+						"5. Put in the oven for 40min (200°C)"]
+		}	
+	},
+	{
+		id: "4",
+		domain: "https://another2domainname",
+		servings: "3",
+		name: "Sweet potato and miso soup",
+		categories: ["healthy", "vegan"],
+		content: {
+			ingredients: ["6 spring onions, chopped", "2 thumb-sized pices ginger", "2 cloves garlic", "2 tbsp white miso", "500g sweet potatoes, peeled and cut into small pieces", "750ml vegtable stock", ],
+			instruction: ["1. Heat 2 tsp of oil in a large lidded pan and gently cook 1/2 the spring onions, ginger and garlic for 5-10 minutes or until soft. Add 1 tbsp of miso, all the sweet potatoes and the vegetable stock, and bring to a simmer. Put on a lid and simmer for 25-30 minutes  or until the sweet potatoes are really soft. Season and whizz with a stick blender until completely smooth. Keep warm.",
+						"2. Heat 2 tsp of oil in a frying pan and fry the remaining spring onions, ginger and garlic over a high heat for 2-3 minutes or until softened. Reduce the heat, add the kale and a splash of water, and cook for 2-3 minutes until the kale has wilted. Stir in the remaining miso and season. Spoon the soup into bowls and top with the miso kale, to serve.",]
+		}	
+	}, 
+	{
+		id: "5",
+		domain: "https://another2domainname",
+		servings: "1",
+		name: "oriental lentil stew",
+		categories: ["healthy", "vegan"],
+		content: {
+			ingredients: ["60g leek", "80g carrots", "100g cauliflower", "70g chopped tomatoes", "0.5 tsp curry powder", "A pinch of coriander", "0.5 tsp paprika powder", "a pinch of chilli powder", "1 piece ginger", "salt&pepper", "325ml water", "35g lentils", "35ml coconut milk"],
+			instruction: ["1. Add ginger, carrots, leek, cauliflower to a pot and saute them for couple min then add the spices and lentils",
+						"2. pour in the water and out the lit on, let it simmer for a couple of minutes",
+						"3. Add chopped tomatoes and coconut milk and let it cook until the vegtables are tender"]
+		}	
+	},
+	 
 ];
